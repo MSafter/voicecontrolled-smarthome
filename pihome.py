92300bc43ffba0ed2f56729b12303a6c32442b6a
Mert Safter <mert.safter@nano-algorithms.com>
2016-12-06 20:42:54 +0100

    implemented check for audio file

--- pihome.py
+++ pihome.py
@@ -4,37 +4,60 @@ from time import sleep
 import requests
 import json
 from configobj import ConfigObj
+import os.path
+import wave
 
 #pyaudio needed to open a audio stream needed for 
 pyAudio = pyaudio.PyAudio()
 headers = {'Content-Type':'application/json'}
 config = None
+commandAudioFile = None
 
 class CommandListener:
 	def __init__(self):
 		#config
-		hmm = config['pocketsphinx']['hmm']
-		lm = config['pocketsphinx']['lm']
-		dict = config['pocketsphinx']['dict']
-		log = config['pocketsphinx']['log']
+		self.hmm = config['pocketsphinx']['hmm']
+		self.lm = config['pocketsphinx']['lm']
+		self.dict = config['pocketsphinx']['dict']
+		self.log = config['pocketsphinx']['log']
 		
 		self.bitsize = int(config['bitsize'])		
 		
 		#set decoder configuration
 		self.config = Decoder.default_config()
-		self.config.set_string('-hmm',hmm)
-		self.config.set_string('-lm',lm)
-		self.config.set_string('-dict',dict)
-		self.config.set_string('-logfn', log)
+		self.config.set_string('-hmm',self.hmm)
+		self.config.set_string('-lm',self.lm)
+		self.config.set_string('-dict',self.dict)
+		self.config.set_string('-logfn', self.log)
 		self.config.set_boolean("-allphone_ci", True)
 				
 		self.decoder = Decoder(self.config)
 		
+		
+	def fromAudio(self):		
+				
+		config = {
+			'hmm':self.hmm,
+			'lm': self.lm,
+			'dict':self.dict
+		}
 
-	def listen(self):
+		ps = Pocketsphinx(**config)
+
+		ps.decode(
+			audio_file= commandAudioFile,
+			buffer_size= 2048,
+			no_search= False,
+			full_utt= False,
+		)
+		
+		return ps.hypothesis()
+
+	def listen(self):		
 		# get stream from pyAudio
-		self.stream = pyAudio.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=self.bitsize)
-									   
+		# open stream 
+		self.stream = pyAudio.open(format=pyaudio.paInt16, channels=2, rate=16000, input=True, frames_per_buffer=self.bitsize)
+				
 		utterance = False
 
 		#start utterance
@@ -43,7 +66,28 @@ class CommandListener:
 		print("Listening...")
 		# now we are starting to listen
 		while True:
-	
+			
+			#check if an external command is used by the user 
+			if os.path.isfile(commandFile):
+				#stop the utterance
+				self.decoder.end_utt()
+				print("external command detected - Processing...")
+				
+				#get the command from the audio file
+				commandFromAudio = self.fromAudio()	
+
+				#check if a command is detected 
+				if not commandFromAudio:
+					commandFromAudio = ""
+					print("No command found in file!")
+				
+				#audio file not needed anymore
+				os.remove(commandFile)
+
+				print("external command processed - Deleting...")
+				#return the command from the audio
+				return commandFromAudio
+		
 			try:
 				soundBite = self.stream.read(self.bitsize)
 			except Exception as e:
@@ -58,7 +102,7 @@ class CommandListener:
 				if inSpeech and not utterance:
 					utterance = True
 				
-				if not inSpeech and utterance:
+				if utterance:
 					#end utterance
 					self.decoder.end_utt()					
 					utterance = False
@@ -68,7 +112,6 @@ class CommandListener:
 					if hypothesis is not None:
 
 						bestGuess = hypothesis.hypstr
-						
 						#check for empty command
 						if not bestGuess.strip():
 							#restart utterance
@@ -84,6 +127,7 @@ class CommandListener:
 
 if __name__ == "__main__":
 	config = ConfigObj('pihome.conf')
+	commandAudioFile = config["command"]["audiofile"]
 	#Listener for the commands the user is speaking out
 	listener = CommandListener()
 
